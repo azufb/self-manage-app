9bd508e0819d12ae4d1d86c1bd0033d9acd05acd
azufb <curiousazunas@gmail.com>
2021-06-10 15:05:41 +0900

    firebase削除

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import {firebaseStore} from './index'
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  const [users, setUsers] = useState("");
-  const admin = require('firebase-admin');
 
-  admin.initializeApp();
-
-  const db = admin.firestore();
-
-  useEffect(() => {
-    const searchUsers = async() => {
-      // Firestoreのコレクションを指定してデータ取得。今回は全量を検索
-      const res = await db.collection('users').get();
-      if (res.empty) return [];
-      const userList　= [];
-      // DocumentData型にはmapメソッドが定義されていないため、forEachのループでデータを加工
-      res.forEach(doc => {
-          userList.push(doc.data());
-      })
-      setUsers(userList);
-    }
-
-    searchUsers();
-    setLoading(false);
-  }, []);
   return (
     <div>
-      {users.map((user, index) =>
-        <p key={index}>{user.name}</p>
-      )}
+      Hello
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,25 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import firebase from "firebase";
-const env = process.env;
-
-export const firebaseConfig = {
-  apiKey: env.REACT_APP_API_KEY,
-  authDomain: env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: env.REACT_APP_DATABASE_URL,
-  projectId: env.REACT_APP_PROJECT_ID,
-  storageBucket: env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: env.REACT_APP_APP_ID,
-  //measurementId: env.REACT_APP_MEASUREMENT_ID,
-};
-
-export const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-//export const firebaseStore = firebaseApp.firestore();
-
-export default firebase;
 
 ReactDOM.render(
   <React.StrictMode>

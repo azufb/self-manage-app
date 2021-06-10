ba091bf59c6b7ee3725ed9dd5599434605ffdafc
azufb <curiousazunas@gmail.com>
2021-06-10 12:25:08 +0900

    env更新

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const env = process.env;
 export const firebaseConfig = {
   apiKey: env.REACT_APP_API_KEY,
   authDomain: env.REACT_APP_AUTH_DOMAIN,
-  //databaseURL: env.REACT_APP_DATABASE_URL,
+  databaseURL: env.REACT_APP_DATABASE_URL,
   projectId: env.REACT_APP_PROJECT_ID,
   storageBucket: env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
@@ -18,10 +18,6 @@ export const firebaseConfig = {
   //measurementId: env.REACT_APP_MEASUREMENT_ID,
 };
 
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: 'https://self-manage-app.firebaseio.com'
-});
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
 

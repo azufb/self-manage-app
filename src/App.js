0afa72cba25ab9cc12057747cd36b56cfe6f372f
azufb <curiousazunas@gmail.com>
2021-06-10 17:55:35 +0900

    Firebaseの利用を一旦中止

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 
 export default function App() {
 
   return (
-    <div>
+    <div className="App-header">
       Hello
     </div>
   );

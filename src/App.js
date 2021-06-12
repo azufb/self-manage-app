84aab2e7481d0e308d9b04d827472cc3f5ea7e2c
azufb <curiousazunas@gmail.com>
2021-06-12 09:46:21 +0900

    ルーティングの設定

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default function App() {
 
   return (
     <div className="App-header">
-      Hello
+      <Router>
+        <div>
+          <h1>
+            <Link to="/">Self-Manage</Link>
+          </h1>
+          <ul>
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/Event">Event</Link>
+            </li>
+          </ul>
+        </div>
+        <Switch>
+          <Route path="/Event">
+            <Event />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

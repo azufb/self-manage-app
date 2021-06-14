7f85c3f32721f7aef2a8625d59154a3bc1a318cd
azufb <curiousazunas@gmail.com>
2021-06-14 11:05:04 +0900

    contextの利用

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 import Navi from "./components/Navi";
+import reducer from "./reducers";
+import AppContext from './contexts/AppContext';
 
-export default function App() {
+const App = () => {
   const initialState = {
     events: []
   }
 
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App-header">
-      <Navi />
-    </div>
+    <AppContext.Provider value={{ state, dispatch }} >
+      <div className="App-header">
+        <Navi />
+      </div>
+    </AppContext.Provider>
   );
 }
+
+export default App;

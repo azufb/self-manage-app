0423b65722d4cd8cd2ca1dde075ff50a2fc07d84
azufb <curiousazunas@gmail.com>
2021-06-16 18:55:21 +0900

    #19 編集機能作成

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,11 +1,13 @@
-import React, { useContext } from "react";
-import { DELETE_EVENT } from "../actions";
+import React, { useContext, useState } from "react";
+import { DELETE_EVENT, EDIT_EVENT } from "../actions";
 import AppContext from "../contexts/AppContext";
 
 const Event = ({event}) => {
   const {dispatch} = useContext(AppContext);
-  //const [title, setTitle] = useState(event.title);
+  const [name, setName] = useState(event.name);
+  const [comment, setComment] = useState(event.comment);
   const id = event.id;
+  const [editable, setEditable] = useState(false);
 
   const handleDeleteEvent = () => {
     dispatch({
@@ -14,15 +16,48 @@ const Event = ({event}) => {
     });
   }
 
+  // editableがtrueであれば、inputエリアを表示。
+  const handleEditable = () => {
+    setEditable(true);
+  }
+
+  const handleEditEvent = () => {
+    dispatch({
+      type: EDIT_EVENT,
+      id,
+      name,
+      comment
+    });
+    setEditable(false);
+  }
+
   return (
-    <tr>
-      <td>{event.id}</td>
-      <td>{event.name}</td>
-      <td>{event.comment}</td>
-      <td>
-        <button type="button" onClick={handleDeleteEvent}>削除</button>
-      </td>
-    </tr>
+    <React.Fragment>
+    {editable ? 
+      (
+        <tr>
+          <td>{event.id}</td>
+          <td><input value={name} onChange={(e) => setName(e.target.value)} /></td>
+          <td><input value={comment} onChange={(e) => setComment(e.target.value)} /></td>
+          <td>
+            <button type="button" onClick={handleEditEvent}>保存</button>
+          </td>
+        </tr>
+      ):(
+        <tr>
+          <td>{event.id}</td>
+          <td>{event.name}</td>
+          <td>{event.comment}</td>
+          <td>
+            <button type="button" onClick={handleDeleteEvent}>削除</button>
+          </td>
+          <td>
+            <button type="button" onClick={handleEditable}>編集</button>
+          </td>
+        </tr>
+      )
+    }
+    </React.Fragment>
   )
 }
 

65729fd7697c2ac441556e07b85a24170f030708
azufb <curiousazunas@gmail.com>
2021-06-16 17:46:51 +0900

    comment機能追加、idを1番始まりに変更

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -4,23 +4,27 @@ import AppContext from "../contexts/AppContext";
 
 const EventForm = () => {
   const { dispatch } = useContext(AppContext); // contextから、dispatchを受け取る。
-  const [title, setTitle] = useState("");
+  const [name, setName] = useState("");
+  const [comment, setComment] = useState("");
 
   const handleAddEvent = (e) => {
     e.preventDefault();
 
     dispatch({
       type: ADD_EVENT,
-      title
+      name,
+      comment
     });
 
-    setTitle("")
+    setName("");
+    setComment("");
   }
   return (
     <div>
       <form>
         <label htmlFor="titleForm">イベントタイトル</label>
-        <input type="text" id="titleForm" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="イベントのタイトルを入力してください。" />
+        <input type="text" id="titleForm" value={name} onChange={(e) => setName(e.target.value)} placeholder="イベント名を入力してください。" />
+        <textarea value={comment} onChange={(e) => setComment(e.target.value)} placeholder="コメントを入力してください。" />
         <button onClick={handleAddEvent}>追加</button>
       </form>
     </div>

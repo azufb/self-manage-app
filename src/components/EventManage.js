3701f9ab615a39196a3f8998d41711bba3a9ecd1
azufb <curiousazunas@gmail.com>
2021-07-09 22:22:02 +0900

    ページに関する説明を更新。

--- src/components/EventManage.js
+++ src/components/EventManage.js
@@ -7,7 +7,10 @@ const EventManage = () => {
   return (
     <div className={styles.contents}>
       <h1>Events</h1>
-      <p>参加したイベントを管理できるページです。イベント名と参加したコメント・感想などを残しましょう！</p>
+      <p>
+        参加したイベントを管理できるページです！<br />
+        早速、下のボタンからイベント名と参加したコメント・感想などを登録し、記録を始めていきましょう！
+      </p>
       <EventForm />
       <EventList />
     </div>

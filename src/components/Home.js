53b79be6e91acf49bc1c8c3f92260d1238c8c4b5
azufb <curiousazunas@gmail.com>
2021-07-09 21:24:35 +0900

    アプリケーション名を修正(現在は、イベント管理のみ提供のため。)

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,11 @@ import styles from "../styles/Home.css";
 const Home = () => {
   return (
     <div className={styles.contents}>
-      <h1>Self Manage</h1>
+      <h1>Event Recorder</h1>
       <p>
-        参加したイベントなど、履歴を残しておきたいものをリスト表示することができます。<br />
-        なんでも、履歴として残して管理してしまいましょう！<br />
-        <span className={styles.annotation}>※現在は、イベント管理機能のみのご提供となっております。今後、そのほかの管理機能は開発中となっております。ご了承ください。</span>
+        参加したイベントを記録することができます！<br />
+        「参加した」という事実だけでなく、コメント・感想などを残すこともできます。参加したイベントで、学んだことなどを記録してみてください！<br></br>
+        <span className={styles.annotation}>※そのほかの管理機能も開発中となっております。お待ちください！</span>
       </p>
     </div>
   )
